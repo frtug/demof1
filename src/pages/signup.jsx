c71667769992bd81b579819b47b9bc5a303b307c
abhisheksharma <sharma.abhi3199@gmail.com>
2025-04-05 20:28:02 +0530

    updated with routes

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './AuthStyles.css';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://api.frtug.site';
 
 
 const Signup = () => {
